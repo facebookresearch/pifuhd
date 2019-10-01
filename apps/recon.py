5f655a966243dd733ebeb43ed26529b4ef69cd4e
Shunsuke Saito <shunsukesaito@shunsukesaito-fedora-it2277287.DHCP.thefacebook.com>
2019-09-30 17:52:35 -0700

    update reconstruction module

---
+++ apps/recon.py
@@ -0,0 +1,191 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+import time
+import json 
+import numpy as np
+import cv2
+import random
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib
+
+from lib.options import BaseOptions
+from lib.visualizer import Visualizer
+from lib.mesh_util import *
+from lib.sample_util import *
+from torchy.data import *
+from torchy.model import *
+from torchy.geometry import index
+
+from PIL import Image
+import torchvision.transforms as transforms
+
+parser = BaseOptions()
+
+def label_to_color(nways, N=10):
+    '''
+    args:
+        nwaysL [N] integer numpy array
+    return:
+        [N, 3] color numpy array
+    '''
+    mapper = cm.ScalarMappable(norm=matplotlib.colors.Normalize(vmin=0, vmax=N), cmap=plt.get_cmap('tab10'))
+
+    colors = []
+    for v in nways.tolist():
+        colors.append(mapper.to_rgba(float(v)))
+
+    return np.stack(colors, 0)[:,:3]
+
+def reshape_multiview_tensors(image_tensor, calib_tensor):
+    '''
+    args:
+        image_tensor: [B, nV, C, H, W]
+        calib_tensor: [B, nV, 3, 4]
+    return:
+        image_tensor: [B*nV, C, H, W]
+        calib_tensor: [B*nV, 3, 4]
+    '''
+    image_tensor = image_tensor.view(
+        image_tensor.shape[0] * image_tensor.shape[1],
+        image_tensor.shape[2],
+        image_tensor.shape[3],
+        image_tensor.shape[4]
+    )
+    calib_tensor = calib_tensor.view(
+        calib_tensor.shape[0] * calib_tensor.shape[1],
+        calib_tensor.shape[2],
+        calib_tensor.shape[3]
+    )
+
+    return image_tensor, calib_tensor
+
+def reshape_sample_tensor(sample_tensor, num_views):
+    '''
+    args:
+        sample_tensor: [B, 3, N] xyz coordinates
+        num_views: number of views
+    return:
+        [B*nV, 3, N] repeated xyz coordinates
+    '''
+    if num_views == 1:
+        return sample_tensor
+    sample_tensor = sample_tensor[:, None].repeat(1, num_views, 1, 1)
+    sample_tensor = sample_tensor.view(
+        sample_tensor.shape[0] * sample_tensor.shape[1],
+        sample_tensor.shape[2],
+        sample_tensor.shape[3]
+    )
+    return sample_tensor
+
+def gen_mesh(res, net, cuda, data, save_path, thresh=0.5, use_octree=False, components=False):
+    image_tensor = data['img'].to(device=cuda)
+    calib_tensor = data['calib'].to(device=cuda)
+
+    net.filter(image_tensor)
+
+    b_min = data['b_min']
+    b_max = data['b_max']
+    try:
+        save_img_path = save_path[:-4] + '.png'
+        save_img_list = []
+        for v in range(image_tensor.shape[0]):
+            save_img = (np.transpose(image_tensor[v].detach().cpu().numpy(), (1, 2, 0)) * 0.5 + 0.5)[:, :, ::-1] * 255.0
+            save_img_list.append(save_img)
+        save_img = np.concatenate(save_img_list, axis=1)
+        cv2.imwrite(save_img_path, save_img)
+
+        verts, faces, _, _ = reconstruction(
+            net, cuda, calib_tensor, res, b_min, b_max, thresh, use_octree=use_octree, num_samples=500000)
+        verts_tensor = torch.from_numpy(verts.T).unsqueeze(0).to(device=cuda).float()
+        if not components:
+            net.calc_normal(verts_tensor, calib_tensor[:1])
+            color = net.nmls.detach().cpu().numpy()[0].T
+        else:
+            nways = net.calc_comp_ids(verts_tensor, calib_tensor[:1])
+            color = label_to_color(nways[0].detach().cpu().numpy())
+        # xyz_tensor = net.projection(verts_tensor, calib_tensor[:1])
+        # uv = xyz_tensor[:, :2, :]
+        # color = index(image_tensor[:1], uv).detach().cpu().numpy()[0].T
+        color = color * 0.5 + 0.5
+        save_obj_mesh_with_color(save_path, verts, faces, color)
+    except Exception as e:
+        print(e)
+
+
+def recon(opt):
+    # load checkpoints
+    state_dict_path = None
+    if opt.load_netG_checkpoint_path is not None:
+        state_dict_path = opt.load_netG_checkpoint_path
+    elif opt.resume_epoch < 0:
+        state_dict_path = '%s/%s_train_latest' % (opt.checkpoints_path, opt.name)
+        opt.resume_epoch = 0
+    else:
+        state_dict_path = '%s/%s_train_epoch_%d' % (opt.checkpoints_path, opt.name, opt.resume_epoch)
+    
+    state_dict = None
+    if state_dict_path is not None and os.path.exists(state_dict_path):
+        print('Resuming from ', state_dict_path)
+        state_dict = torch.load(state_dict_path)    
+        if 'opt' in state_dict:
+            print('Warning: opt is overwritten.')
+            dataroot = opt.dataroot
+            no_numel_eval = opt.no_numel_eval
+            no_mesh_recon = opt.no_mesh_recon
+            opt = state_dict['opt']
+            opt.dataroot = dataroot
+            opt.no_numel_eval = no_numel_eval
+            opt.no_mesh_recon = no_mesh_recon
+    else:
+        raise Exception('failed loading state dict!', state_dict_path)
+    
+    parser.print_options(opt)
+
+    cuda = torch.device('cuda:%d' % opt.gpu_id)
+
+    test_dataset = EvalDataset(opt)
+
+    print('test data size: ', len(test_dataset))
+    projection_mode = test_dataset.projection_mode
+
+    netG = HGPIFuNet(opt, projection_mode).to(device=cuda)
+
+    def set_eval():
+        netG.eval()
+
+    # load checkpoints
+    if state_dict is not None:
+        if 'model_state_dict' in state_dict:
+            netG.load_state_dict(state_dict['model_state_dict'])
+        else: # this is deprecated but keep it for now.
+            netG.load_state_dict(state_dict)
+
+    os.makedirs(opt.checkpoints_path, exist_ok=True)
+    os.makedirs(opt.results_path, exist_ok=True)
+    os.makedirs('%s/%s/eval' % (opt.results_path, opt.name), exist_ok=True)
+
+    ## test
+    with torch.no_grad():
+        set_eval()
+
+        print('generate mesh (test) ...')
+        for test_data in tqdm(test_dataset):
+            save_path = '%s/%s/result_%s.obj' % (opt.results_path, opt.name, test_data['name'])
+            gen_mesh(opt.resolution, netG, cuda, test_data, save_path, components=opt.use_compose)
+
+def reconWrapper(args=None):
+    opt = parser.parse(args)
+    recon(opt)
+
+if __name__ == '__main__':
+    reconWrapper()
+  
--- torchy/data/EvalDataset.py
+++ torchy/data/EvalDataset.py
@@ -21,19 +21,11 @@ class EvalDataset(Dataset):
         self.projection_mode = projection
 
         self.root = self.opt.dataroot
-        self.RENDER = os.path.join(self.root, 'RENDER')
-        self.MASK = os.path.join(self.root, 'MASK')
-        self.PARAM = os.path.join(self.root, 'PARAM')
-        self.OBJ = os.path.join(self.root, 'GEO', 'OBJ')
+        self.img_files = [os.path.join(self.root,f) for f in os.listdir(self.root) if '.png' in f]
+        self.IMG = os.path.join(self.root)
 
         self.phase = 'val'
         self.load_size = self.opt.loadSize
-
-        self.num_views = self.opt.num_views
-
-        self.max_view_angle = 360
-        self.interval = 1
-        self.subjects = self.get_subjects()
 
         # PIL to tensor
         self.to_tensor = transforms.Compose([
@@ -41,124 +33,31 @@ class EvalDataset(Dataset):
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
-    
-    def get_subjects(self):
-        var_file = os.path.join(self.root, 'val.txt')
-        if os.path.exists(var_file):
-            var_subjects = np.loadtxt(var_file, dtype=str)
-            return sorted(list(var_subjects))
-        all_subjects = os.listdir(self.RENDER)
-        return sorted(list(all_subjects))
-
 
     def __len__(self):
-        return len(self.subjects) * self.max_view_angle // self.interval
-
-    def get_render(self, subject, num_views, pitch=0, view_id=None, random_sample=False):
-        '''
-        Return render data
-        args:
-            subject: subject name
-            num_views: number of views
-            pitch: pitch angle (default: 0)
-            view_id: the first view id. if None, select randomly
-        return:
-            'img': None, # [num_views, C, H, W] input images
-            'calib': None, # [num_views, 4, 4] calibration matrix
-            'extrinsic': None, # [num_views, 4, 4] extrinsic matrix
-            'mask': None, # [num_views, 1, H, W] segmentation masks
-        '''
-        if view_id is None:
-            view_id = np.random.randint(self.max_view_angle)
-        # views are sampled evenly unless random_sample is enabled
-        view_ids = [(view_id + self.max_view_angle // num_views * offset) % self.max_view_angle
-                    for offset in range(num_views)]
-        if random_sample:
-            view_ids = np.random.choices(self.max_view_angle, num_views, replace=False)
-
-        calib_list = []
-        render_list = []
-        mask_list = []
-        extrinsic_list = []
-
-        for vid in view_ids:
-            param_path = os.path.join(self.PARAM, subject, '%d_%d_%02d.npy' % (vid, pitch, 0))
-            render_path = os.path.join(self.RENDER, subject, '%d_%d_%02d.jpg' % (vid, pitch, 0))
-            mask_path = os.path.join(self.MASK, subject, '%d_%d_%02d.jpg' % (vid, pitch, 0)) 
-
-            # load calibration data
-            param = np.load(param_path, allow_pickle=True)
-            # pixel unit / world unit
-            ortho_ratio = param.item().get('ortho_ratio')
-            # world unit / model unit
-            scale = param.item().get('scale')
-            # camera center world coordinate
-            center = param.item().get('center')
-            # model rotation
-            R = param.item().get('R')
-
-            translate = -np.matmul(R, center).reshape(3, 1)
-            extrinsic = np.concatenate([R, translate], axis=1)
-            extrinsic = np.concatenate([extrinsic, np.array([0, 0, 0, 1]).reshape(1, 4)], 0)
-            # make sure camera space matches with pixel coordinates
-            scale_intrinsic = np.identity(4)
-            scale_intrinsic[0, 0] = scale / ortho_ratio
-            scale_intrinsic[1, 1] = -scale / ortho_ratio # due to discripancy between OpenGL and CV
-            scale_intrinsic[2, 2] = scale / ortho_ratio
-            # transformation from pixe space to normalized space [0, 1]
-            ndc_intrinsic = np.identity(4)
-            ndc_intrinsic[0, 0] = 1.0 / float(self.opt.loadSize // 2)
-            ndc_intrinsic[1, 1] = 1.0 / float(self.opt.loadSize // 2)
-            ndc_intrinsic[2, 2] = 1.0 / float(self.opt.loadSize // 2)
-            # transformation in normalized coordinates
-            trans_intrinsic = np.identity(4)
-
-            mask = Image.open(mask_path).convert('L')
-            render = Image.open(render_path).convert('RGB')
-
-            intrinsic = np.matmul(trans_intrinsic, np.matmul(ndc_intrinsic, scale_intrinsic))
-            calib = torch.Tensor(np.matmul(intrinsic, extrinsic)).float()
-            extrinsic = torch.Tensor(extrinsic).float()
-
-            mask = transforms.Resize(self.load_size)(mask)
-            mask = transforms.ToTensor()(mask).float()
-            mask_list.append(mask)
-
-            render = self.to_tensor(render)
-            render = mask.expand_as(render) * render
-
-            render_list.append(render)
-            calib_list.append(calib)
-            extrinsic_list.append(extrinsic)
-
-        return {
-            'img': torch.stack(render_list, dim=0),
-            'calib': torch.stack(calib_list, dim=0),
-            'extrinsic': torch.stack(extrinsic_list, dim=0),
-            'mask': torch.stack(mask_list, dim=0)
-        }
+        return len(self.img_files)
 
     def get_item(self, index):
-        # in case of IO error, use random sampling instead
-        subject = ''
-        try:
-            sid = index % len(self.subjects)
-            vid = (index // len(self.subjects)) * self.interval
-            # name of the subjects 'rp_xxxx_xxx'
-            subject = self.subjects[sid]
-            res = {
-                'name': subject,
-                'mesh_path': os.path.join(self.OBJ, subject + '.obj'),
-                'sid': sid,
-                'vid': vid,
-            }
-            render_data = self.get_render(subject, num_views=self.num_views, view_id=vid,
-                                          random_sample=self.opt.random_multiview)
-            res.update(render_data)
-            return res
-        except Exception as e:
-            print(e)
-            return self.get_item(index=random.randint(0, self.__len__() - 1))
+        img_path = self.img_files[index]
+        # Name
+        img_name = os.path.splitext(os.path.basename(img_path))[0]
+        # Calib
+        B_MIN = np.array([-1, -1, -1])
+        B_MAX = np.array([1, 1, 1])
+        projection_matrix = np.identity(4)
+        projection_matrix[1, 1] = -1
+        calib = torch.Tensor(projection_matrix).float()
+
+        # image
+        image = Image.open(img_path).convert('RGB')
+        image = self.to_tensor(image)
+        return {
+            'name': img_name,
+            'img': image.unsqueeze(0),
+            'calib': calib.unsqueeze(0),
+            'b_min': B_MIN,
+            'b_max': B_MAX,
+        }
 
     def __getitem__(self, index):
         return self.get_item(index)
