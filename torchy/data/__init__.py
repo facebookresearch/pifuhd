50726a63633dc23dffb1fab9e5fb05ec6084a2de
Shunsuke Saito <shunsukesaito@shunsukesaito-fedora-it2277287.DHCP.thefacebook.com>
2019-10-25 15:53:30 -0700

    added frl face loader

--- torchy/data/__init__.py
+++ torchy/data/__init__.py
@@ -5,3 +5,4 @@ from .RPDatasetParts import RPDatasetParts
 from .RPOtfDatasetParts import RPOtfDatasetParts
 from .RPOtfDataset import RPOtfDataset
 from .RPTSDFDataset import RPTSDFDataset
+from .FRLFaceDataset import FRLFaceDataset
